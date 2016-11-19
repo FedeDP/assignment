41b3b0f3477ed5a6381100a716f6a524f67a2d69
Federico Di Pierro <nierro92@gmail.com>
2016-11-19 11:49:26 +0100

    improved printf inside tests.

--- src/test.c
+++ src/test.c
@@ -47,7 +47,7 @@ int main(void) {
  * Test that a not-existent options file returns -1
  */
 static void test_not_existent_options_file(void) {
-    printf("Testing not existent options file.\n");
+    printf("***Testing not existent options file.***\n");
     strcpy(conf.options_file, "?"); 
     
     assert(read_conf(&conf) == -1);
@@ -57,7 +57,7 @@ static void test_not_existent_options_file(void) {
  * Test existent empty options file
  */
 static void test_empty_options(void) {
-    printf("Testing empty options file.\n");
+    printf("***Testing empty options file.***\n");
     strcpy(conf.options_file, "test/test_options_blank.conf");
     
     assert(read_conf(&conf) == -1);
@@ -67,7 +67,7 @@ static void test_empty_options(void) {
  * Test existent wrong options file
  */
 static void test_wrong_options(void) {
-    printf("Testing wrong options file.\n");
+    printf("***Testing wrong options file.***\n");
     strcpy(conf.options_file, "test/test_options_wrong.txt"); 
     
     assert(read_conf(&conf) == -1);
@@ -79,7 +79,7 @@ static void test_wrong_options(void) {
  * Test existent options file
  */
 static void test_options_file(void) {
-    printf("Testing existent options file.\n");
+    printf("***Testing existent options file.***\n");
     strcpy(conf.options_file, "test/test_options.conf");
         
     assert(read_conf(&conf) == 0);
@@ -91,7 +91,7 @@ static void test_options_file(void) {
  * Test not existent input file
  */
 static void test_not_existent_input_file(void) {
-    printf("Testing not existent input file.\n");
+    printf("***Testing not existent input file.***\n");
     strcpy(conf.input_file, "?"); 
     
     assert(read_input_file(conf, &h, &size) == -1);
@@ -102,7 +102,7 @@ static void test_not_existent_input_file(void) {
  * Test existent empty input file
  */
 static void test_empty_input(void) {
-    printf("Testing empty input file.\n");
+    printf("***Testing empty input file.***\n");
     strcpy(conf.input_file, "test/test_input_blank.conf"); 
     
     assert(read_input_file(conf, &h, &size) == -1);
@@ -113,7 +113,7 @@ static void test_empty_input(void) {
  * Test existent input file
  */
 static void test_input_file(void) {
-    printf("Testing existent input file.\n");
+    printf("***Testing existent input file.***\n");
     strcpy(conf.input_file, "test/test_input.txt"); 
     
     assert(read_input_file(conf, &h, &size) == 0);
@@ -125,7 +125,7 @@ static void test_input_file(void) {
  * Test expected impedance. test_input has only one line: {4.00, 3.00}, so we expect 5.00
  */
 static void test_impedance(void) {
-    printf("Testing expected impedance value of 5.00.\n");
+    printf("***Testing expected impedance value of 5.00.***\n");
     
     assert(compute_impedance(h, size) == 5.00);
 }
